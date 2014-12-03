f00e0b0f3de5fddcfaafdac8fb4b78c149aa2822
Joao Cravo <joao.cravo@blip.pt>
2014-12-03 00:03:22 +0000

    WIP #2 - Added documentation to gocdapi.__init__ and agent modules.

--- gocdapi/__init__.py
+++ gocdapi/__init__.py
@@ -2,32 +2,64 @@
 About this library
 ==================
 
-TODO
+Go is a continuous delivery system - http://www.go.cd/
+The goal of this python library is to be an easy interface for the Go server.
+This project was inspirated in the jenkinsapi library - https://github.com/salimfadhley/jenkinsapi
+
+This library can help you:
+
+* Ability to list Go Agents
+* Ability to enable/disable/delete a Go Agent
+* Ability to list Go Pipelines Groups and their pipelines
+* Ability to schedule/release lock/pause/unpause a Go Pipeline
+
 
 Installing gocdapi
 =====================
 
-TODO
+While there isn't a stable release hosted in PyPi, you can clone the repository and run:
+
+python setup.py install
+
 
 Project Authors
 ===============
 
  * Joao Cravo (joaogbcravo@gmail.com)
 
-Current code lives on github: TODO
+Current code lives on github: https://github.com/joaogbcravo/gocdapi
 
 """
 import pkg_resources
 from gocdapi import (
 
     # Files
-    agent, agents, configuration, custom_exceptions, go, gobase, pipeline, pipeline_group,
-    pipeline_groups, stage
+    admin,
+    agent,
+    agents,
+    configuration,
+    custom_exceptions,
+    go,
+    gobase,
+    pipeline,
+    pipeline_group,
+    pipeline_groups,
+    stage
 )
 
 __all__ = [
-    "agent", "agents", "configuration", "custom_exceptions", "go", "gobase", "pipeline", "pipeline_group",
-    "pipeline_groups", "stage"
+    "admin",
+    "agent",
+    "agents",
+    "configuration",
+    "custom_exceptions",
+    "go",
+    "gobase",
+    "pipeline",
+    "pipeline_group",
+    "pipeline_groups",
+    "stage"
 ]
+
 __docformat__ = "epytext"
 __version__ = pkg_resources.working_set.by_key['gocdapi'].version
--- gocdapi/agent.py
+++ gocdapi/agent.py
@@ -1,5 +1,5 @@
 """
-Module for gocdapi agent
+Module for gocdapi Agent class
 """
 
 from gocdapi.gobase import GoBase
@@ -11,41 +11,97 @@ class Agent(GoBase):
     """
 
     def __init__(self, go_server, data):
+        """Inits Agent objects.
+
+        Args:
+            go_server (Go): A Go object which this agent belongs to.
+            data (str): A json string representing the agent configuration
+        """
         self._data = None
         super(self.__class__, self).__init__(go_server, data=data)
 
+    def __str__(self):
+        """Returns a pretty representation of the object
+
+        Returns:
+            str: representation of the object
+        """
+        return 'Agent @ %s' % self.go_server.baseurl
+
     def poll(self):
+        """Will create and define the attributes of the agent.
+
+        Uses _data attribute populated by inherited methods, updating object attributes using the bunch pattern.
+        Also sets the agent url.
+        """
         self.__dict__.update(self._data)
         self.set_self_url('go/api/agents/%s/' % self.uuid)
 
     def repoll_from_agents_url(self):
+        """Request to the server new agents configuration to upload agent object
+
+        Because Go Server haven't any API to return a single Agent configuration, this function will do a GET request
+        to get information about all the agents, and then update the actual object with the right information.
+        """
         agents_data = self.get_json_data(self.build_url_with_base('go/api/agents'))
         self._data = next(data for (index, data) in enumerate(agents_data) if data["uuid"] == self.uuid)
         self.__dict__.update(self._data)
 
-    def is_disabled(self):
-        return self.status == "Disabled"
-
     def is_enabled(self):
+        """Check if agent is enabled
+
+        Does a repoll first to get updated data.
+
+        Returns:
+            bool: True if not Disabled
+        """
         return self.status != "Disabled"
 
     def enable(self):
+        """Enables the agent
+
+        Will do a POST request to go/api/agents/UUID/enable
+        Does a repoll after the request for enabling the agent.
+        """
+        return self.status != "Disabled"
         url = self.build_url('enable')
         self.do_post(url)
         self.repoll_from_agents_url()
 
     def disable(self):
+        """Disables the agent
+
+        Will do a POST request to go/api/agents/UUID/disable
+        Does a repoll after the request for enabling the agent.
+        """
         url = self.build_url('disable')
         self.do_post(url)
         self.repoll_from_agents_url()
 
     def delete(self):
+        """Deletes the agent
+
+        Will do a POST request to go/api/agents/UUID/delete
+        """
         url = self.build_url('delete')
         self.do_post(url)
 
     def job_run_history(self, offset=0):
+        """Gets the history of the jobs run on the agent
+
+        Go server returns 10 instances at a time, sorted in reverse order.
+        You can use offset argument which tells the API how many instances to skip.
+
+        Will do a GET request to go/api/agents/UUID/job_run_history/OFFSET
+
+        Args:
+            offset (int): how many instances to skip
+
+        Returns:
+            str: json object representing job history
+
+        """
         url = self.build_url('job_run_history/%s' % offset)
         return self.get_json_data(url)
 
-    def __str__(self):
-        return 'Agent @ %s' % self.go_server.baseurl
+
